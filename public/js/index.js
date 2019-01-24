7c83ea2cba2274848ecd02f080b03b4b230c1c40
Ahmed Bitar <Ahmad>
2019-01-24 21:02:24 +0200

    =Front-end

--- public/js/index.js
+++ public/js/index.js
@@ -9,23 +9,38 @@ var socket = io('http://localhost:3000');
   });
 
   socket.on('newMessage', function (message) {
+    var template = jQuery('#message-template').html();
     var formattedTime  = moment(message.createdAt).format('h:mm a');
-    console.log('New message', message);
-    var li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
-    jQuery('#messages').append(li);
+    var html = Mustache.render(template, {
+      text:message.text,
+      from:message.from,
+      createdAt:formattedTime
+    });
+
+    jQuery('#messages').append(html);
+    // console.log('New message', message);
+    // var li = jQuery('<li></li>');
+    // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+    // jQuery('#messages').append(li);
   });
 
   socket.on('newLocationMessage', function (message) {
     var formattedTime  = moment(message.createdAt).format('h:mm a');
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template, {
+      from:message.from,
+      createdAt:formattedTime,
+      url:message.url
+    });
 
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My Current Location</a>');
-    li.text(`${message.from}: ${formattedTime} `);
-    a.attr('href', message.url)
-    li.append(a);
-    jQuery('#messages').append(li);
-
+    jQuery('#messages').append(html);
+    
+    // var li = jQuery('<li></li>');
+    // var a = jQuery('<a target="_blank">My Current Location</a>');
+    // li.text(`${message.from}: ${formattedTime} `);
+    // a.attr('href', message.url)
+    // li.append(a);
+    // jQuery('#messages').append(li);
   });
 
   jQuery('#message-form').on('submit',  function (e) {
